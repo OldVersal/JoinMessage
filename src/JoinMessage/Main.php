e68f5357cee6d426089125d1629ccec7600601b0
OldVersal <137354711+OldVersal@users.noreply.github.com>
2023-07-21 03:48:20 -0600

    Create Main.php

---
+++ src/JoinMessage/Main.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace JoinMessage;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+
+        $this->getLogger()->info("JoinMessage ha sido activado");
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    }
+
+    public function onDisable(): void  {
+
+        $this->getLogger()->info("JoinMessage ha sido deshabilitado");
+
+    }
+
+    // Ejemplo de evento: Cuando un jugador se une al servidor.
+
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+
+        $player = $event->getPlayer();
+
+        $player->sendMessage(TextFormat::colorize("&8[&a+&8]" . $player->getName() . ""));
+
+    }
+
+}
